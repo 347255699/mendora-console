e6fe51a566eb61f16c16600fe0cc0369bb20bcc0
menfre <menfre@wokoworks.com>
2019-04-26 18:35:15 +0800

    提交修改

--- supervisor-console/src/main/java/org/mendora/SupervisorConsoleApplication.java
+++ supervisor-console/src/main/java/org/mendora/SupervisorConsoleApplication.java
@@ -1,7 +1,5 @@
 package org.mendora;
 
-import io.vertx.core.VertxOptions;
-import io.vertx.core.file.FileSystemOptions;
 import io.vertx.reactivex.core.Vertx;
 import org.apache.logging.log4j.Level;
 
@@ -10,21 +8,19 @@ import org.apache.logging.log4j.Level;
  *
  * @author menfre
  */
-public class SupervisorConsoleApplication {
+public class SupervisorConsoleApplication extends VertxApplicationInit{
+	public static Vertx vertx;
+
 	public static void main(String[] args) {
 		LoggerModule.builder()
 			.logLevel(Level.DEBUG)
 			.build()
 			.run();
-		System.setProperty("vertx.logger-delegate-factory-class-name", "io.vertx.core.logging.SLF4JLogDelegateFactory");
-		VertxOptions vertxOptions = new VertxOptions()
-			.setFileSystemOptions(new FileSystemOptions().setFileCachingEnabled(false))
-			.setWorkerPoolSize(10);
-		Vertx vertx = Vertx.vertx(vertxOptions);
+		vertx = vertx(false, 10);
 		System.setProperty("uploadDir", "/webserver/other/file-uploads");
 		WebModule.builder()
 			.webRoot("/webserver/other/")
-			.port(83)
+			.port(8080)
 			.build()
 			.run(vertx, "org.mendora.route");
 	}
--- supervisor-console/src/main/java/org/mendora/route/ConsoleRoute.java
+++ supervisor-console/src/main/java/org/mendora/route/ConsoleRoute.java
@@ -7,19 +7,37 @@ import io.vertx.reactivex.core.http.HttpServerRequest;
 import io.vertx.reactivex.core.http.HttpServerResponse;
 import io.vertx.reactivex.ext.web.FileUpload;
 import io.vertx.reactivex.ext.web.RoutingContext;
+import lombok.Builder;
 import lombok.extern.slf4j.Slf4j;
+import org.jsoup.Jsoup;
+import org.jsoup.nodes.Document;
+import org.jsoup.nodes.Element;
+import org.jsoup.select.Elements;
 import org.mendora.facade.RequestRouting;
 import org.mendora.facade.RespCode;
 import org.mendora.facade.Route;
 import org.mendora.facade.RouteFactory;
 
 import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 @Route("/console")
 @Slf4j
 public class ConsoleRoute implements RouteFactory {
 	private String uploadDir = System.getProperty("uploadDir");
+
+	@Builder
+	private static class ProcessInfo {
+		private String status;
+		private String desc;
+		private String name;
+		private String action;
+	}
 
 	public enum RespErrorCode implements RespCode {
 		/**
@@ -59,16 +77,40 @@ public class ConsoleRoute implements RouteFactory {
 		Single.just(fileUploads)
 			.filter(set -> set.size() == 1)
 			.map(set -> set.iterator().next())
-			.map(file -> {
-				String tempFileName = file.uploadedFileName();
-				String fileName = file.fileName();
-				return reName(tempFileName, fileDir + "/" + fileName).blockingGet();
-			})
+			.map(file -> rxRename(file.uploadedFileName(), fileDir + "/" + file.fileName()).blockingGet())
 			.subscribe(result -> succ(response, result), err -> log.error(err.getMessage(), err))
 			.dispose();
 	}
 
-	private Maybe<Boolean> reName(String originPath, String destPath) {
+	@RequestRouting(value = "/process", blocked = true)
+	public void loadingProcess(RoutingContext rtx) throws IOException {
+		HttpServerResponse response = rtx.response();
+		Document document = Jsoup.connect("http://localhost:9001/index.html").get();
+		final Elements tbody = document.body().getElementsByTag("tbody");
+		final Iterator<Element> trIterator = tbody.iterator();
+		List<Element> trs = new ArrayList<>();
+		while (trIterator.hasNext()) {
+			trs.add(trIterator.next());
+		}
+		List<ProcessInfo> processInfos = trs.stream()
+			.map(tr -> {
+				final Iterator<Element> tdIterator = tr.children().iterator();
+				List<Element> tds = new ArrayList<>();
+				while (tdIterator.hasNext()) {
+					tds.add(tdIterator.next());
+
+				}
+				return ProcessInfo.builder()
+					.status(tds.get(0).html())
+					.desc(tds.get(1).html())
+					.name(tds.get(2).html())
+					.action(tds.get(3).html())
+					.build();
+			}).collect(Collectors.toList());
+		succ(response, processInfos);
+	}
+
+	private Maybe<Boolean> rxRename(String originPath, String destPath) {
 		File origin = new File(originPath);
 		String destDirName = destPath.substring(0, destPath.lastIndexOf("/") + 1);
 		return Single.just(new File(destDirName))
